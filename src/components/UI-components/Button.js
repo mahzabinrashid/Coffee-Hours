c1e421e37fac5118f5a61d7bc90feee01c6b8677
Emily Y <emilyyst19@hotmail.com>
2021-08-28 18:02:21 -0400

    Form styling
    - form styling using semantic-ui

--- src/components/UI-components/Button.js
+++ src/components/UI-components/Button.js
@@ -2,9 +2,24 @@ import React from "react";
 import "./Button.css"
 
 class Button extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.class = "button"
+    
+    if (this.props.secondary)
+      this.class += " secondary"
+    else
+      this.class  += " primary"
+      
+    if (this.props.big) {
+      this.class += " big"
+    }
+  }
+
   render() {
     return(
-      <button className={this.props.secondary ? "secondary" : "primary"}>
+      <button className={this.class}>
         {this.props.text}
       </button>
     )
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,5 +1,10 @@
+import Button from "../UI-components/Button.js"
+import { Input } from "semantic-ui-react";
 import { useState } from "react";
 import fire from "../../fire";
+
+import "./auth.css"
+
 export default function SignIn() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -18,25 +23,27 @@ export default function SignIn() {
       .then((cred) => console.log(cred.user));
   };
   return (
-    <div>
+    <div className="form">
       <h1>Sign In</h1>
       <form onSubmit={formSubmission}>
-        <div>
+        <div className="group">
           <label>Email</label>
-          <input
+          <Input
             type="text"
             onChange={emailInputChangeHandler}
-          ></input>
+            required
+          />
         </div>
-        <div>
+        <div className="group">
           <label>Password</label>
-          <input
+          <Input
             type="text"
             onChange={passwordInputChangeHandler}
-          ></input>
+            required
+          />
         </div>
         <div>
-          <button>Submit</button>
+          <Button text="Submit" />
         </div>
       </form>
     </div>
