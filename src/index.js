67d660176a39e345f15a2caa0689a4e41e5e067c
Mahzabin Rashid Fariha <mahzabinrashidfariha@gmail.com>
2021-09-02 07:01:37 -0400

    no css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import 'semantic-ui-css/semantic.min.css'
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 
 ReactDOM.render(
